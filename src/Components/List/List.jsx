2618921c857f7a948b2f6dc84fdbaa05246eefa6
Stephanie Hill <stephni.hill@gmail.com>
2022-11-01 09:51:46 -0700

    delete

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -3,11 +3,11 @@ import { useContext } from 'react';
 import { SettingsContext } from '../../Context/Settings';
 import { useState } from 'react';
 import { Pagination } from '@mantine/core';
-import { Card, Text, Badge, Button, Group } from '@mantine/core';
-
+import { Card, Text, Badge, Button, Group, Menu, ActionIcon } from '@mantine/core';
+import { IconDots, IconTrash } from '@tabler/icons';
 const List = ({ children }) => {
 
-  const { list, toggleComplete } = useContext(SettingsContext);
+  const { list, toggleComplete, deleteItem } = useContext(SettingsContext);
   const [activePage, setPage] = useState(1);
   
 
@@ -20,6 +20,18 @@ const List = ({ children }) => {
             <Group position="apart" mt="md" mb="xs">
             <Badge color="green" variant="light">Pending</Badge>
             <Text weight={300}>Assigned to: {item.assignee}</Text>
+            <Menu withinPortal position="bottom-end" shadow="sm">
+            <Menu.Target>
+              <ActionIcon>
+                <IconDots size={16} />
+              </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item onClick={() => deleteItem(item.id)} icon={<IconTrash size={14} />} color="red">
+                Delete
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
             </Group>
             <Text size="sm" color="dimmed">{item.text}</Text>
             <Text size="sm" color="dimmed">Difficulty: {item.difficulty}</Text>
