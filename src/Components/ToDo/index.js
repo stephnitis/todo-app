eb052abe2440bd5a8cb0822eb97540753b92d49e
Stephanie Hill <stephni.hill@gmail.com>
2022-10-31 21:35:03 -0700

    this stupid form

--- src/Components/ToDo/index.js
+++ src/Components/ToDo/index.js
@@ -1,5 +1,6 @@
-import {useContext} from 'react';
+import { useContext } from 'react';
 import { SettingsContext } from '../../Context/Settings';
+import { Grid } from '@mantine/core';
 import AddForm from '../AddForm/AddForm.jsx';
 import List from '../List/List'
 import './todo.scss'
@@ -10,11 +11,20 @@ const ToDo = () => {
 
   return (
     <>
-     <header data-testid="todo-header">
-        <h1 data-testid="todo-h1" id="todo-header">To Do List: {incomplete} items pending</h1>
-      </header>
-    <AddForm />
-    <List />  
+
+        <header data-testid="todo-header">
+          <h1 data-testid="todo-h1" id="todo-header">To Do List: {incomplete} items pending</h1>
+        </header>
+
+      <Grid justify="center">
+        <Grid.Col span={2}>
+          <AddForm />
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <List />
+        </Grid.Col>
+      </Grid>
+
     </>
   );
 };
