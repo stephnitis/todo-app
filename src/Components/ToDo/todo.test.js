901b21a143e83cef0b3759d3a53a56eb2b845546
Stephanie Hill <stephni.hill@gmail.com>
2022-11-02 10:26:30 -0700

    one passing

--- src/Components/ToDo/todo.test.js
+++ src/Components/ToDo/todo.test.js
@@ -2,11 +2,15 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
 import ToDo from './index';
+import  SettingsProvider  from '../../Context/Settings'
 
-
-describe('ToDo Component Tests', ()  => {
+describe('ToDo Component Tests', () => {
   test('render a header element as expected', () => {
-    render(<ToDo />);
+    render(
+      <SettingsProvider>
+          <ToDo />
+      </SettingsProvider>
+    );
 
     let header = screen.getByTestId('todo-header');
     // let h1 = screen.getByTestId('todo-h1');
